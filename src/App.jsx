daa07d1fcbc3eb952317c4f3eac256f76c41c350
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-09-08 15:57:20 +0200

    [feat] create pages and add routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import AboutUs from "./pages/AboutUs";
-import ProductsPages from "./pages/ProductsPages";
+import AboutUsPage from "./pages/AboutUsPage";
+import ProductsListPage from "./pages/ProductsListPage";
 import DefaultLayout from "./layouts/DefaultLayout";
-import DetailProductPage from "./pages/DetailProductPage";
-
 
 function App() {
   return (
@@ -12,10 +10,9 @@ function App() {
       <Routes>
         <Route element={<DefaultLayout />}>
           <Route path="/" element={<HomePage />} ></Route>
-          <Route path="/about-us" element={<AboutUs />} ></Route>
+          <Route path="/about-us" element={<AboutUsPage />} ></Route>
           <Route path="/products">
-            <Route index element={<ProductsPages />} ></Route>
-            <Route path=":id" element={<DetailProductPage />} ></Route>
+            <Route index element={<ProductsListPage />} ></Route>
           </Route>
         </Route>
       </Routes >
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 
-
 const HomePage = () => {
   return (
     <div className='container'>
       <div className="row">
         <div className="col-12">
-          <h1>Home page</h1>
+          <h1>Benvenuto nell'e-commerce</h1>
         </div>
       </div>
     </div>
