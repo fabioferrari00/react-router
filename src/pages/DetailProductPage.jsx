d638ae8c8f75e88abf40dec9a4cd63b5656f8869
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-09-08 13:26:52 +0200

    [feat] first and second steps

--- src/pages/DetailProductPage.jsx
+++ src/pages/DetailProductPage.jsx
@@ -2,27 +2,29 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 const DetailProductPage = () => {
   const { id } = useParams()
   const [product, setProduct] = useState({})
-  const [products, setProducts] = useState([]);
-  console.log(useParams())
 
   useEffect(() => {
-    axios.get(`https://fakestoreapi.com/products`).then((resp) => {
-      setProducts(resp.data)
+    axios.get(`https://fakestoreapi.com/products/${id}`).then((resp) => {
+      setProduct(resp.data)
     })
 
-    const singleProduct = products.find((product) => product.id === parseInt(id))
-    setProduct(singleProduct)
   }, [])
 
   return (
     <div className='container'>
       <div className="row">
-        <div className="col-12">
+        <div className="col-12 text-center">
           <h1>{product.title}</h1>
+          <img src={product.image} alt="" />
+          <p>- {product.price}&euro;</p>
+          <p>- {product.category}</p>
+          <p>- {product.description}</p>
+          <Link to="/products" className='btn btn-primary'>Torna a tutti i prodotti</Link>
         </div>
       </div>
     </div>
